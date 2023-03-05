3c9116ba10f1a69f44935d971418157896fa30e3
Đł Ɽ₳łⱫɆⱠ ㋡ <122148266+acex69@users.noreply.github.com>
2023-03-05 21:56:00 +0530

    lancelot: drop gms

--- aosp_lancelot.mk
+++ aosp_lancelot.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Gapps
-$(call inherit-product, vendor/gapps/common/common-vendor.mk)
+
+
 
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
