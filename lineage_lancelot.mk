613410d18a1c13ffade612622011c9fe59d4999c
acex69 <abhinav.115260@gmail.com>
2023-03-06 23:16:03 +0530

    bringup_alphadroid

--- lineage_lancelot.mk
+++ lineage_lancelot.mk
@@ -8,16 +8,31 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-
-
-
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common Alphadroid stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080 
+TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := aosp_lancelot
+# Alphadroid Stuffs 
+TARGET_HAS_UDFPS := false 
+TARGET_ENABLE_BLUR := true 
+TARGET_INCLUDE_MATLOG := false 
+TARGET_USE_PIXEL_LAUNCHER := false 
+TARGET_EXCLUDES_AUDIOFX := true 
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Gapps 
+WITH_GAPPS := true 
+TARGET_OPTOUT_GOOGLE_TELEPHONY := true
+
+# Blaze Maintainer 
+ALPHA_MAINTAINER := raizel 
+ALPHA_BUILD_TYPE := Unofficial
+
+PRODUCT_NAME := lineage_lancelot
 PRODUCT_DEVICE := lancelot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
